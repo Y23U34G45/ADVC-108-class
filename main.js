527f9fc077b0503e7968d0cd6c01b6dd8baf4b49
Y23U34G45 <99113923+Y23U34G45@users.noreply.github.com>
2022-02-19 13:51:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
 function startClassification()
 {
-    navigator.mediaDevices,getUserMedia({audio: true});
-    classfier.soundClassifier('https://teachablemachine.withgoogle.com/models/OVk9S4udE/model.json', modelReady) ;
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier.soundClassifier('https://teachablemachine.withgoogle.com/models/OVk9S4udE/model.json', modelReady) ;
 
 }
 function modelReady(){
-    classfier.classify(gotResult);
+    classifier.classify(gotResults);
 
 }
-function gotResult(error, result)
-{
+function gotResult(error, results){
     if (error){
         console.error(error);
     }else{
-        console.log(result);
+        console.log(results);
         random_number_r = Math.floor(Math.random() * 255) + 1;
         random_number_g = Math.floor(Math.random() * 255) + 1;
         random_number_b = Math.floor(Math.random() * 255) + 1;
 
-        document.getElementById("result_label").innerHTML = 'I can hear - '
-        result[0].label;
-        document.getElementById("result_confidence").innerHTML = 'Accuracy - '
-        (result[0].confidence*100).toFixed(2)+" %";
-        document.getElementById("result_label").style.color = "rgb("
-        +random_number_r+","+random_number_g+","+random_number_b+")";
-        document.getElementById("result_confidence").style.color = "rgb("
-        +random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_label").innerHTML = 'I can hear - ' + results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_b+")";
 
         img = document.getElementById('alien1');
         img1 = document.getElementById('alien2');
